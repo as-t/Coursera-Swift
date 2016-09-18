4db2d2a2003a4e991cb9e345c49d6f3d9a50927d
David Kruger <me@krugerdavid.com>
2016-09-18 18:06:25 -0400

    Tarea realizada 1er Reto Programacion iOS Swift - Coursera

---
+++ primerRetoSwift.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for i in 0...100 {
+    
+    if( i >= 30 && i <= 40 ) {
+        print("\(i) Viva Swift")
+    } else if( i % 2 == 0 ) {
+        print("\(i) Par")
+    } else if( i % 3 == 0 ) {
+        print("\(i) Impar")
+    } else if( i % 5 == 0 )  {
+        print("\(i) Bingo")
+    } else {
+        print("\(i)")
+    }
+}
+
+
